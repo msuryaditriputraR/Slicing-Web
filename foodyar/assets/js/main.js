74033f8bd707c40b76f518c5f07b4a0ef6775575
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-12-30 09:13:21 +0800

    feat: active link

--- foodyar/assets/js/main.js
+++ foodyar/assets/js/main.js
@@ -53,6 +53,30 @@ navToggle.addEventListener("click", () => {
     navList.classList.toggle("active-menu");
 });
 
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute("id");
+        const sectionClass = document.querySelector(
+            `.nav__menu a[href*=${sectionId}]`
+        );
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionClass.classList.add("active-link");
+        } else {
+            sectionClass.classList.remove("active-link");
+        }
+    });
+}
+
+window.addEventListener("scroll", scrollActive);
+
 /* ==================== UTILS ===================== */
 function removeAllState(elem, state) {
     elem.forEach((el) => el.classList.remove(state));
