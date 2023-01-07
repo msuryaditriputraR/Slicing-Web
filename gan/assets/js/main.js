1c8eb3c0b6b762427ab07112c4941a63b3c7597e
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-01-07 17:19:54 +0800

    add: swiper slider

---
+++ gan/assets/js/main.js
@@ -0,0 +1,22 @@
+/* ==================== TEAMS SLIDER ===================== */
+const swiper = new Swiper(".home3__teams", {
+    effect: "coverflow",
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 2,
+    coverflowEffect: {
+        rotate: 0,
+        modifier: 2.5,
+        stretch: 20,
+        slideShadows: false,
+        scale: 0.95,
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
