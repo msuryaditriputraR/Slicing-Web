3bd97e8c25181f04774cf38ffab639ffb718402e
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-01-25 19:23:05 +0800

    add: dragable for documentation cards

--- BKM/assets/js/main.js
+++ BKM/assets/js/main.js
@@ -5,43 +5,46 @@ function dragToScroll(elemID) {
 
     let pos = { top: 0, left: 0, x: 0, y: 0 };
 
-    const mouseDownHandler = function (e) {
-        pos = {
-            // The current scroll
-            left: elem.scrollLeft,
-            top: elem.scrollTop,
-            // Get the current mouse position
-            x: e.clientX,
-            y: e.clientY,
+    if (elem) {
+        const mouseDownHandler = function (e) {
+            pos = {
+                // The current scroll
+                left: elem.scrollLeft,
+                top: elem.scrollTop,
+                // Get the current mouse position
+                x: e.clientX,
+                y: e.clientY,
+            };
+
+            // Change the cursor and prevent user from selecting the text
+            elem.style.cursor = "grabbing";
+            elem.style.userSelect = "none";
+
+            elem.addEventListener("mousemove", mouseMoveHandler);
+            elem.addEventListener("mouseup", mouseUpHandler);
         };
 
-        // Change the cursor and prevent user from selecting the text
-        elem.style.cursor = "grabbing";
-        elem.style.userSelect = "none";
+        const mouseMoveHandler = function (e) {
+            // How far the mouse has been moved
+            const dx = e.clientX - pos.x;
+            const dy = e.clientY - pos.y;
 
-        elem.addEventListener("mousemove", mouseMoveHandler);
-        elem.addEventListener("mouseup", mouseUpHandler);
-    };
+            // Scroll the element
+            elem.scrollTop = pos.top - dy;
+            elem.scrollLeft = pos.left - dx;
+        };
 
-    const mouseMoveHandler = function (e) {
-        // How far the mouse has been moved
-        const dx = e.clientX - pos.x;
-        const dy = e.clientY - pos.y;
+        const mouseUpHandler = function () {
+            elem.removeEventListener("mousemove", mouseMoveHandler);
+            elem.removeEventListener("mouseup", mouseUpHandler);
 
-        // Scroll the element
-        elem.scrollTop = pos.top - dy;
-        elem.scrollLeft = pos.left - dx;
-    };
+            elem.style.cursor = "grab";
+            elem.style.removeProperty("user-select");
+        };
 
-    const mouseUpHandler = function () {
-        elem.removeEventListener("mousemove", mouseMoveHandler);
-        elem.removeEventListener("mouseup", mouseUpHandler);
-
-        elem.style.cursor = "grab";
-        elem.style.removeProperty("user-select");
-    };
-
-    elem.addEventListener("mousedown", mouseDownHandler);
+        elem.addEventListener("mousedown", mouseDownHandler);
+    }
 }
 
 dragToScroll("clients-cards");
+dragToScroll("documentation-cards");
